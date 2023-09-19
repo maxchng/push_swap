b23801e6a5d900e05924727aeea8715c5f0053e7
Yuen Zheng Chng <ychng@u81z08s04.42kl.edu.my>
2023-09-19 21:21:55 +0800

    Added is_not_sorted.

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,25 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 14:48:10 by ychng             #+#    #+#             */
-/*   Updated: 2023/09/19 03:28:22 by ychng            ###   ########.fr       */
+/*   Updated: 2023/09/19 21:20:59 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	is_not_sorted(t_linked_list *stack_a)
+{
+	t_node	*current_a;
+
+	current_a = stack_a->head;
+	while (current_a->next)
+	{
+		if (current_a->data > current_a->next->data)
+			return (1);
+		current_a = current_a->next;
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -23,15 +37,19 @@ int	main(int argc, char **argv)
 	if (argc >= 2)
 	{
 		parse_argv(&stack_a, argc, argv);
-		size_a = count_stack_size(&stack_a);
-		if (size_a == 2)
-			sort_two(&stack_a);
-		else if (size_a == 3)
-			sort_three(&stack_a, &stack_b);
-		else if (size_a == 5)
-			sort_five(&stack_a, &stack_b);
-		else if (size_a > 10)
-			sort_big(&stack_a, &stack_b);
+		
+		if (is_not_sorted(&stack_a))
+		{
+			size_a = count_stack_size(&stack_a);
+			if (size_a == 2)
+				sort_two(&stack_a);
+			else if (size_a == 3)
+				sort_three(&stack_a, &stack_b);
+			else if (size_a == 5)
+				sort_five(&stack_a, &stack_b);
+			else if (size_a > 10)
+				sort_big(&stack_a, &stack_b);
+		}
 	}
 	free_stack(&stack_a);
 	free_stack(&stack_b);
